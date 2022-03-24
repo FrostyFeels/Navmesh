5368657d80342fc838b60d8f834026bde614b719
remcospil1 <remcospil1@gmail.com>
2022-03-24 16:05:18 +0100

    Work from school week 3

--- Navmesh/Assets/FlankerAi.cs
+++ Navmesh/Assets/FlankerAi.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class FlankerAi : MonoBehaviour
 {
-    [SerializeField] private LayerMask layer;
+    [SerializeField] private LayerMask layer, outerLayer;
 
     public Transform player;
     public Vector3 homePoint;
+    public float distanceFromHomepoint;
+    public float maxDistance;
 
     public float minimalDistance;
 
@@ -30,46 +32,88 @@ public class FlankerAi : MonoBehaviour
     public int currentNode;
     public int lastNode;
 
-
+    public float minimalNeededDistance;
+    public float distanceFromNode;
     
     
     void Start()
     {
         _DistanceToFirstNode = 0;
         _FlankNodes = new GameObject[8];
+        _AvailableNodes = new Vector3[8];
         for (int i = 0; i < _FlankNodes.Length; i++)
         {        
             _FlankNodes[i] = Instantiate(prefab);
+            
         }
+
+        homePoint = player.transform.position;
+        DrawFlankNodes();
+        FindClosetsNode();
+        CheckHomePointAvailability();
     }
 
     // Update is called once per frame
     void Update()
     {
-        homePoint = player.transform.position;
-
-        DrawFlankNodes();
+        CheckHomePointAvailability();
         FindClosetsNode();
-        CheckAvailableNodes();
-
 
     }
 
-
-
+    //Draw 8 squares each coming from the home point
     public void DrawFlankNodes()
     {
         for (int i = 0; i < _FlankNodes.Length; i++)
         {
-            Vector3 endPoint = homePoint + (_FlankDirections[i] * flankNodeDistance);
 
+            Vector3 endPoint = homePoint + (_FlankDirections[i] * flankNodeDistance);
+            RaycastHit2D hit = Physics2D.Linecast(homePoint, endPoint, outerLayer);
+
+            if(hit)
+            {
+                _FlankNodes[i].transform.position = hit.point;
+            }
+            else
+            {
+                _FlankNodes[i].transform.position = endPoint;
+            }
             Debug.DrawLine(homePoint, endPoint, Color.black);
 
-            _FlankNodes[i].transform.position = endPoint;
+          
             
             
         }
     }
+
+
+    //Code for checking if the node can see the player without walls
+    //Color the nodes red if they have vision or blue if vision is blocked
+    public void CheckAvailableNodes()
+    {
+        for (int i = 0; i < _FlankNodes.Length; i++)
+        {
+            RaycastHit2D hit = Physics2D.Linecast(homePoint, _FlankNodes[i].transform.position, layer);
+            float distanceToNode = Vector3.Distance(player.transform.position, _FlankNodes[i].transform.position);
+
+            if (_FlankNodes[i] != closetNode)
+            {
+                if (!hit && distanceToNode >= minimalNeededDistance)
+                {
+                    Debug.DrawRay(homePoint, _FlankDirections[i], Color.green);
+                    _FlankNodes[i].GetComponent<SpriteRenderer>().color = Color.red;
+
+                }
+                else
+                {
+                    Debug.Log(hit.collider);
+                    _FlankNodes[i].GetComponent<SpriteRenderer>().color = Color.cyan;
+                }
+            }
+        }
+    }
+
+    //Code to find the closest node you want the A.I to move too
     public void FindClosetsNode()
     {
         lastNode = currentNode;
@@ -106,22 +150,17 @@ public class FlankerAi : MonoBehaviour
         }
         
     }
-    public void CheckAvailableNodes()
-    {
-        for (int i = 0; i < _FlankNodes.Length; i++)
-        {
-            RaycastHit2D hit = Physics2D.Linecast(homePoint, _FlankNodes[i].transform.position, layer);
-            if (!hit)
-            {
-                Debug.DrawRay(homePoint, _FlankDirections[i], Color.green);
-                _FlankNodes[i].GetComponent<SpriteRenderer>().color = Color.red;                    
-            }
-            else
-            {
-                Debug.Log(hit.collider);
-                _FlankNodes[i].GetComponent<SpriteRenderer>().color = Color.cyan;
-            }
 
+    //Check if the player hasn't moved to far away from the homepoint and reset the homepoints position to the player
+    public void CheckHomePointAvailability()
+    {
+        distanceFromHomepoint = Vector3.Distance(homePoint, player.transform.position);
+
+        if(distanceFromHomepoint > maxDistance)
+        {
+            homePoint = player.transform.position;
+            DrawFlankNodes();
+            CheckAvailableNodes();  
         }
     }
 }
